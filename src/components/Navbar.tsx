ece76d996c56e2758ce05624a80295c62f129f9d
Sevi <correosevi@gmail.com>
2023-11-11 23:16:49 +0000

    Feat | conditional rendering with session

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,29 +2,46 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { signIn, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const { data: session } = useSession();
-  console.log(session);
   return (
     <nav className="bg-slate-900 flex items-center justify-between px-24 py-3  text-white">
       <Link href="/">
         <h1>Next Auth0</h1>
       </Link>
 
-      <div className="flex gap-x-2 items-center">
-        <Link href="/dashboard">Dashboard</Link>
+      {session?.user ? (
+        <div className="flex gap-x-2 items-center">
+          <Link href="/dashboard">Dashboard</Link>
+          <p>
+            {session.user.name} {session.user.email}
+          </p>
 
+          <Image
+            src={session.user?.image ?? '/default-user-image.png'}
+            alt="User Image"
+            width={30}
+            height={30}
+            className="rounded-full w-8 h-8"
+          />
+          <button
+            onClick={() => signOut()}
+            className="bg-sky-400 px-3 py-2 rounded-md text-white"
+          >
+            Sign Out
+          </button>
+        </div>
+      ) : (
         <button
           onClick={() => signIn()}
           className="bg-sky-400 px-3 py-2 rounded-md text-white"
         >
           Login
         </button>
-      </div>
+      )}
     </nav>
   );
 };
