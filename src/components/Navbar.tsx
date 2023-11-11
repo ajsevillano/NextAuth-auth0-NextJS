eae6b33b272f38c14d97c4e941fcaaa686ac12e2
Sevi <correosevi@gmail.com>
2023-11-11 22:34:57 +0000

    Feat | create basic navigation and signin button

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import { signIn } from 'next-auth/react';
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="bg-slate-900 flex items-center justify-between px-24 py-3  text-white">
+      <Link href="/">
+        <h1>Next Auth0</h1>
+      </Link>
+
+      <div className="flex gap-x-2 items-center">
+        <Link href="/dashboard">Dashboard</Link>
+
+        <button
+          onClick={() => signIn()}
+          className="bg-sky-400 px-3 py-2 rounded-md text-white"
+        >
+          Login
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
